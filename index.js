7319146b0d9a39d34a8dcb7efdb07809f78164ee
MatDup6080 <mathieu.dupressoir03@gmail.com>
2025-10-10 16:52:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import {formatDateLong} from "/composant/fonction/date.js";
 // Déclaration des variables globales
 // -----------------------------------------------------------------------------------
 
-/* global prochaineEdition, lesClassements*/
+/* global prochaineEdition, lesClassements, informations*/
 
 // Récupération des éléments de l'interface
 const detailClassement = document.getElementById('detailClassement');
@@ -19,6 +19,8 @@ const dateEpreuve = document.getElementById('dateEpreuve');
 const descriptionEpreuve = document.getElementById('descriptionEpreuve');
 const btnOuvrirToutes = document.getElementById('btnOuvrirToutes');
 const btnFermerToutes = document.getElementById('btnFermerToutes');
+const information = document.getElementById('information');
+
 
 // -----------------------------------------------------------------------------------
 // Procédures évènementielles
@@ -27,36 +29,42 @@ const btnFermerToutes = document.getElementById('btnFermerToutes');
 btnOuvrirToutes.onclick = () => basculerToutesLesCartes(true);
 btnFermerToutes.onclick = () => basculerToutesLesCartes(false); // fermer
 
-
 // -----------------------------------------------------------------------------------
 // Programme principal
 // -----------------------------------------------------------------------------------
 
-// Mise en place du système d'ouverture/fermeture des cadres
+
 initialiserToutesLesCartes();
 
-// les informations
+// Affichage des informations
+for (const element of informations) {
+    let div = document.createElement('div');
+    div.classList.add('information-item');
+    div.innerHTML = `<h3>${element.titre}</h3><p>${element.contenu}</p><small>Par ${element.auteur}</small>`;
 
-// affichage de la prochaine épreuve
-dateEpreuve.innerText =  formatDateLong(prochaineEdition.date);
+    // Si l'information est privée, l'ajouter dans la section privée
+    if (element.type === 'privee') {
+        // Afficher la section privée si elle était cachée
+        document.getElementById('sectionPrivee').style.display = 'block';
+        document.getElementById('informationPrivee').appendChild(div);
+    } else {
+        // Information publique
+        information.appendChild(div);
+    }
+}
+
+// Rendre visible la section informations
+information.style.display = 'block';
+
+// Affichage de la prochaine épreuve
+dateEpreuve.innerText = formatDateLong(prochaineEdition.date);
 descriptionEpreuve.innerHTML = prochaineEdition.description;
 
-
-// afficher les derniers classements pdf
+// Afficher les derniers classements pdf
 for (const element of lesClassements) {
     let a = document.createElement('a');
-    a.classList.add('lien'),
-        a.href = "/afficherclassement.php?id=" + element.id;
+    a.classList.add('lien');
+    a.href = "/afficherclassement.php?id=" + element.id;
     a.innerText = element.dateFr + ' ' + element.titre;
     detailClassement.appendChild(a);
 }
-
-
-
-
-
-
-
-
-
-
